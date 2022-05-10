637f4b1f47afa8ead968c58c6931e972f6c49784
jovejar <jovejar@gmail.com>
2022-05-10 18:06:15 -0400

    formulario que guarda datos

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'pages/index'
   root 'pages#index'
+  get 'pages/create'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
