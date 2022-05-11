873260b6cf5bf0559a01755f72ff439a6767946e
jovejar <jovejar@gmail.com>
2022-05-11 18:44:02 -0400

    form1

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
     validates :email, presence: true
+    validates :age, numericality: { greater_than: 17, less_than: 151 }
+    scope :find_valid, -> { where("age > ?", 20)}
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  resources :posts
   get 'pages/index'
   root 'pages#index'
   get 'pages/create'
+  get '/pages/dashboard'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
