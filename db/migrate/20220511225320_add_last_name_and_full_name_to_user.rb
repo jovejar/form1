4b8dc252239b92a9d1637d6e8a50c43fc4966319
jovejar <jovejar@gmail.com>
2022-05-11 19:06:11 -0400

    agregar nombre

---
+++ db/migrate/20220511225320_add_last_name_and_full_name_to_user.rb
@@ -0,0 +1,6 @@
+class AddLastNameAndFullNameToUser < ActiveRecord::Migration[7.0]
+  def change
+    add_column :users, :last_name, :string
+    add_column :users, :full_name, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_11_220213) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_11_225320) do
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "age"
     t.text "comment"
+    t.string "name", default: "NN"
+    t.string "last_name"
+    t.string "full_name"
   end
 
 end
